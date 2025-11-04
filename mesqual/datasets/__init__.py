a67b85dc884ea8514cc22ed98e6eb03722f34d0b
helgestefan <on@hstes.de>
2025-11-04 09:58:25 +0100

    adds __all__ to datasets.__init__

--- mesqual/datasets/__init__.py
+++ mesqual/datasets/__init__.py
@@ -9,3 +9,17 @@ from mesqual.datasets.dataset_collection import (
 from mesqual.datasets.dataset_comparison import DatasetComparison, DatasetConcatCollectionOfComparisons
 from mesqual.datasets.platform_dataset import PlatformDataset
 from mesqual.datasets.dataset_config import DatasetConfig
+
+
+__all__ = [
+    'Dataset',
+    'DatasetCollection',
+    'DatasetLinkCollection',
+    'DatasetMergeCollection',
+    'DatasetSumCollection',
+    'DatasetConcatCollection',
+    'DatasetComparison',
+    'DatasetConcatCollectionOfComparisons',
+    'PlatformDataset',
+    'DatasetConfig',
+]
